547a4d5a0a4b2d2bc9f1437276aed61f2d34c1c9
GianlucaBar <gianlucabarce@gmail.com>
2021-03-23 13:57:43 +0100

    completato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 var numero;
-for(var i=1; i<=100; i++){
+for(var i=1; i <= 100; i++){
     numero = i;
     if( numero % 3 == 0 && numero % 5 == 0 ) {
-        console.log('FizzBuzz')
+        console.log('FizzBuzz');
     } else if( numero % 5 == 0 ){
-        console.log('Buzz')
+        console.log('Buzz');
     } else if ( numero % 3 == 0 ){
-        console.log('Fizz')
+        console.log('Fizz');
     } else{
-        console.log(numero)
+        console.log(numero);
     }
 }
